df3189bbecfa979e2ece777e1a6431c6afda49a4
Tazub Singh <tazubsingh@Tazubs-MacBook-Air.local>
2026-03-29 16:22:15 +0530

    Commit after changes

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
